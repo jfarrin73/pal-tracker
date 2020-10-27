65b358be21a2b1a696e8b95763338ad752c2f49e
jfarrin73 <jfarrin73@gmail.com>
2020-10-27 20:08:30 +0000

    deployed pal-tracker:v1

---
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -0,0 +1,19 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    private String message;
+
+    public WelcomeController(@Value("${welcome.message}") String msg){
+        this.message = msg;
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+        return message;
+    }
+}
