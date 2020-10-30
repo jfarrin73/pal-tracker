78d7123655468bbbd5e3f1d8fd7944b6f8244392
jfarrin73 <jfarrin73@gmail.com>
2020-10-30 16:47:30 +0000

    spring MVC REST Controller

---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -0,0 +1,8 @@
+package test.pivotal.pal.tracker;
+
+import io.pivotal.pal.tracker.TimeEntryRepository;
+
+public class TimeEntryController {
+    public TimeEntryController(TimeEntryRepository timeEntryRepository) {
+    }
+}
